ab74de6d437d18fd3976058101ff7a30f0d4aa5b
Hasibe-lee <sdhicb@126.com>
2022-01-30 10:56:16 +0800

    完成个人中心功能开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,7 +41,7 @@ function renderAvatar(user) {
   if (user.user_pic !== null) {
     // 渲染图片头像
     $('.layui-nav-img').attr('src', user.user_pic).show();
-    $('text-avatar').hide();
+    $('.text-avatar').hide();
   } else {
     $('.layui-nav-img').hide();
     // 文本头像
---
+++ assets/js/user_pwd.js
@@ -0,0 +1,39 @@
+$(function () {
+  let form = layui.form;
+  let layer = layui.layer;
+
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+    ],
+    samepwd: function (value) {
+      let oldpwd = $('.layui-form [name="oldPwd"').val();
+      if (value === oldpwd) {
+        return '不能与原密码相同!!'
+      }
+    },
+
+    repwd: function (value) {
+      let newpwd = $('.layui-form [name="newPwd"').val();
+      if (value !== newpwd) {
+        return '与新密码不一致！'
+      }
+    }
+  })
+
+  $('.layui-form').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: '/my/updatepwd',
+      data: $(this).serialize(),
+      success: res => {
+        if (res.status !== 0) {
+          return layer.msg('更新密码失败！');
+        }
+        layer.msg('更新成功！')
+        $(this)[0].reset();
+      }
+    })
+  })
+})
